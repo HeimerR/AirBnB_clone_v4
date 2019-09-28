6b587b9fe9eb03c68f802c2ce85041f70af8f63a
klupzor <lopezgand@gmail.com>
2019-09-28 16:57:07 +0000

    Reading check box values

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,3 +1,20 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    let checked = {};
+    $('input[type=checkbox').change(
+        function(){
+            if (this.checked) {
+                checked[($(this).attr("data-id"))] = $(this).attr("data-name");
+            }
+            else{
+                delete checked[($(this).attr("data-id"))];
+            }
+            if (Object.keys(checked).length === 0){
+                $(".amenities h4").html('&nbsp;');
+            }
+            else{
+                console.log(checked);
+                $(".amenities h4").text(Object.values(checked))
+            }
+            
+        });
 });
