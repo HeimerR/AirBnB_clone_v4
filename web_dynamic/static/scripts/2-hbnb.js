7e967c1330091388285120c7f3ee39cfe02d0958
klupzor <lopezgand@gmail.com>
2019-09-28 20:09:54 +0000

    Connect with api

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+    $.ajax({
+        url: 'http://' + location.hostname + ':5001/api/v1/status/',
+        type: 'GET',
+        success: function (data) { 
+            if (data.status === 'OK')
+            {
+                $("DIV#api_status").addClass("available");
+            }
+         }
+      });
+    
+    let checked = {};
+    $('input[type=checkbox').change(
+        function(){
+            if (this.checked) {
+                checked[($(this).attr("data-id"))] = $(this).attr("data-name");
+            }
+            else{
+                delete checked[($(this).attr("data-id"))];
+            }
+            if (Object.keys(checked).length === 0){
+                $(".amenities h4").html('&nbsp;');
+            }
+            else{
+                console.log(checked);
+                $(".amenities h4").text(Object.values(checked))
+            }
+            
+        });
+});
