95978ccecfcb18e449894a726f6f48121fcd8c44
klupzor <lopezgand@gmail.com>
2019-09-29 00:57:45 +0000

    Finishing task 4

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,20 +1,17 @@
-$( document ).ready(function() {
-    let checked = {};
-    $('input[type=checkbox').change(
-        function(){
-            if (this.checked) {
-                checked[($(this).attr("data-id"))] = $(this).attr("data-name");
-            }
-            else{
-                delete checked[($(this).attr("data-id"))];
-            }
-            if (Object.keys(checked).length === 0){
-                $(".amenities h4").html('&nbsp;');
-            }
-            else{
-                console.log(checked);
-                $(".amenities h4").text(Object.values(checked))
-            }
-            
-        });
+$(document).ready(function () {
+  const checked = {};
+  $('input[type=checkbox').change(
+    function () {
+      if (this.checked) {
+        checked[($(this).attr('data-id'))] = $(this).attr('data-name');
+      } else {
+        delete checked[($(this).attr('data-id'))];
+      }
+      if (Object.keys(checked).length === 0) {
+        $('.amenities h4').html('&nbsp;');
+      } else {
+        console.log(checked);
+        $('.amenities h4').text(Object.values(checked));
+      }
+    });
 });
